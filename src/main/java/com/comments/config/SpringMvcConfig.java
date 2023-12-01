32088884ef54ee6621a714d54011cb3a7b4b0446
dengzuxuan <dengzuxuan20010404@126.com>
2023-12-02 02:57:17 +0800

    [feat](USER LOGIN):1.新增两层过滤器 第一层用于刷新token过期时间 第二层过滤

--- src/main/java/com/comments/config/SpringMvcConfig.java
+++ src/main/java/com/comments/config/SpringMvcConfig.java
@@ -1,6 +1,7 @@
 package com.comments.config;
 
 import com.comments.utils.LoginInterceptor;
+import com.comments.utils.RefreshInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -20,15 +21,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SpringMvcConfig implements WebMvcConfigurer {
     @Autowired
     StringRedisTemplate stringRedisTemplate;
+    //配置双层拦截器，第一层拦截器用于刷新token时间，第二层用于拦截user是否正确
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate)).excludePathPatterns(
+        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns(
                 "/shop/**",
                 "/shop-type/**",
                 "/upload/**",
                 "/blog/hot",
                 "/user/code",
                 "/user/login"
-        );
+        ).order(1);
+
+        registry.addInterceptor(new RefreshInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
     }
 }
--- src/main/java/com/comments/utils/LoginInterceptor.java
+++ src/main/java/com/comments/utils/LoginInterceptor.java
@@ -29,38 +29,12 @@ import static com.comments.utils.RedisConstants.LOGIN_USER_TTL;
  */
 public class LoginInterceptor implements HandlerInterceptor {
 
-    // 由于该类型不是spring托管，因此无法注入redisTemplate 只能手动构造
-    StringRedisTemplate stringRedisTemplate;
-    public LoginInterceptor(StringRedisTemplate upredisTemplate) {
-        stringRedisTemplate = upredisTemplate;
-    }
-
-    //请求前
-    //1.更新redis中token的过期时间
-    //2.获取user
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String token = request.getHeader("authorization");
-        if(token==null){
+        if(UserHolder.getUser()==null){
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
-
-        String tokenKey = LOGIN_USER_KEY+token;
-        Map<Object, Object> loginUserMap = stringRedisTemplate.opsForHash().entries(tokenKey);
-        if(loginUserMap.isEmpty()){
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;
-        }
-        UserDTO loginUser = BeanUtil.fillBeanWithMap(loginUserMap, new UserDTO(), false);
-        UserHolder.saveUser(loginUser);
-
-        stringRedisTemplate.expire(tokenKey,LOGIN_USER_TTL, TimeUnit.MINUTES);
         return true;
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        UserHolder.removeUser();
     }
 }
