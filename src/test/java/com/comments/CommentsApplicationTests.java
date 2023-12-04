a46824b6eca7dde96b200b18381d2ec3b9707a90
dengzuxuan <dengzuxuan20010404@126.com>
2023-12-04 21:22:05 +0800

    [feat](SHOP):1.使用逻辑过期解决缓存击穿

--- src/test/java/com/comments/CommentsApplicationTests.java
+++ src/test/java/com/comments/CommentsApplicationTests.java
@@ -1,9 +1,19 @@
 package com.comments;
 
+import com.comments.service.impl.ShopServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class CommentsApplicationTests {
+    @Resource
+    ShopServiceImpl service;
 
+    @Test
+    void testSaveShop() {
+        service.setShopRedisData(10L,10L);
+    }
 
 }
